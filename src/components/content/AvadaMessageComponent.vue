028a52b51780d40d7f20177fcac7bf9002f9165f
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-06 12:35:45 +0200

    creazione avadaMessacomponent

---
+++ src/components/content/AvadaMessageComponent.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="avada-message">
+
+        <div class="container text-center">
+            <h5 class="text-uppercase fw-semibold">Avada Forum is the place to be</h5>
+            <p>In quis lectus sed leo elementum faucibus in dapibus dictum.</p>
+            <p> Nulla molestie tortor nec lectus, venenatis, sed blandit dui finibus.</p>
+            <p> dolor at, bibendum purus. Donec vehicula nec tortor ac finibus.</p>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AvadaMessageComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/style/variables.scss';
+
+.avada-message {
+    background-color: $tr-custom-grey;
+
+    padding: 100px 0;
+
+    h5 {
+        color: $tr-cape-cod;
+        font-size: .7rem;
+        letter-spacing: 5px;
+        margin-bottom: 1rem;
+    }
+
+    p {
+        font-weight: 300;
+        color: $tr-nevada;
+        margin: 0;
+    }
+
+}
+</style>
