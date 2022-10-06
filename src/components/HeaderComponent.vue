0ef7820809789ec074736f5c52ed90120aa4078b
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-06 11:27:12 +0200

    impostazione header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,29 @@
 <template>
     <header>
 
+        <nav class="container navbar text-white">
+
+            <div class="navbar-brand">
+                <img src="@/assets/images/logo.png" alt="avada-logo">
+            </div>
+
+            <ul class="navbar-nav w-50 flex-row justify-content-between">
+
+                <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Apple</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Microsoft</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Android</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Forums</a></li>
+                <li class="nav-item"><a class="nav-link" href="#">Contact us</a></li>
+                <li class="nav-item"><button class="text-uppercase btn btn-dark">Join us</button></li>
+                <li class="nav-item d-flex align-items-center">
+                    <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+                </li>
+
+            </ul>
+
+        </nav>
+
     </header>
 </template>
 
@@ -14,7 +37,11 @@ export default {
 @import '@/assets/style/variables.scss';
 
 header {
-    min-height: 150px;
     background-color: $tr-header-bg;
+
+    .nav-link {
+        color: inherit;
+    }
+
 }
 </style>
