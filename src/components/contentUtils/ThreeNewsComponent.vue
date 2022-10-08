f2dcf8d79220b05a6e5c0883f3a55662cdb58eb8
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-08 23:31:29 +0200

    correct spacing three-in-one

--- src/components/contentUtils/ThreeNewsComponent.vue
+++ src/components/contentUtils/ThreeNewsComponent.vue
@@ -2,15 +2,18 @@
 
     <div class="three-in-one">
 
-        <div v-for="(article) in articles" :key="article.imgName" class="col-3">
+        <div v-for="(article) in articles" :key="article.imgName" class="col-4">
 
             <img :src="require(`@/assets/images/${article.imgName}`)" alt="img">
 
-            <div class="title">{{article.title}}</div>
+            <div class="title fw-bold">{{article.title}}</div>
 
             <div class="articleInfo d-flex">
-            <div class="date">{{article.articleDate}}</div>
-            <div class="comments">Comments {{article.articleComments}}</div>
+
+                <div class="date">{{article.articleDate}}</div>
+                <div class="comments">Comments {{article.articleComments}}</div>
+
+            </div>
             <div class="incipit-text">{{article.incipitText}}</div>
 
         </div>
@@ -30,10 +33,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/assets/style/variables.scss';
+
 // Contenitore di tre elementi
 .three-in-one {
     display: flex;
     justify-content: space-between;
+    gap: $tr-gap-10;
+
+    img {
+        width: calc(100% - ($tr-gap-10 * 2));
+    }
+
+    .articleInfo {
+
+        .date {
+
+            // Barra divisoria tra data e commenti
+            &::after {
+                content: '|';
+                padding: 0 5px;
+            }
+
+        }
+
+    }
 
 }
 </style>
