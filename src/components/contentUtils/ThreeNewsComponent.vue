de2cd55d647bd621b58f03a517a12a2e4af2e227
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-08 23:49:47 +0200

    text decoration

--- src/components/contentUtils/ThreeNewsComponent.vue
+++ src/components/contentUtils/ThreeNewsComponent.vue
@@ -6,12 +6,15 @@
 
             <img :src="require(`@/assets/images/${article.imgName}`)" alt="img">
 
-            <div class="title fw-bold">{{article.title}}</div>
+            <div class="title text-black fw-bold">{{article.title}}</div>
 
             <div class="articleInfo d-flex">
 
                 <div class="date">{{article.articleDate}}</div>
-                <div class="comments">Comments {{article.articleComments}}</div>
+                <div class="comments" :class="{ off : article.articleComments === 'Off'}">
+                    <span>Comments</span>
+                    <span>{{article.articleComments}}</span>
+                </div>
 
             </div>
             <div class="incipit-text">{{article.incipitText}}</div>
@@ -41,6 +44,8 @@ export default {
     justify-content: space-between;
     gap: $tr-gap-30;
 
+    color: $tr-nevada;
+
     img {
         width: 100%;
     }
@@ -55,6 +60,30 @@ export default {
                 padding: 0 5px;
             }
 
+        }
+
+        .comments {
+            color: black;
+            display: flex;
+            flex-direction: row-reverse;
+
+            font-weight: 600;
+
+            :nth-child(2) {
+                padding-right: 5px;
+            }
+
+            &.off {
+                color: inherit;
+                flex-direction: row;
+
+                font-weight: inherit;
+
+                :nth-child(1) {
+                    padding-right: 5px;
+                }
+
+            }
         }
 
     }
