f34d5bebb18c9b80c3ceb2307ff0ab76bf41fa5f
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-09 21:32:23 +0200

    creazione blackBannerComponent

---
+++ src/components/contentUtils/BlackBannerComponent.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="py-5">
+        <a href="#">
+            {{ text }}
+            <font-awesome-icon class="ps-2" icon="fa-solid fa-right-long" />
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BlackBannerComponent',
+    props: {
+        text: String
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/assets/style/variables.scss';
+
+div {
+    background-color: $tr-shark;
+    color: white;
+
+    text-align: center;
+    text-transform: uppercase;
+    font-weight: 600;
+
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+
+}
+</style>
