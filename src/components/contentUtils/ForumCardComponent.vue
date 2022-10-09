cc2116f61cd6b445d9b7793112876cd5a3dfc461
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-10 00:55:22 +0200

    completamento stile forumCardComponent

--- src/components/contentUtils/ForumCardComponent.vue
+++ src/components/contentUtils/ForumCardComponent.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="card p-4">
+    <div class="card p-4 rounded-0 border-0">
 
         <div class="icon-container text-center p-4">
             <font-awesome-icon class="icon p-4 rounded-circle" :icon="forumInfo.icon" />
@@ -24,6 +24,10 @@ export default {
 
 .card {
 
+    // Uso di important per sovrascrivere bootstrap rules
+    padding-bottom: 75px !important;
+    border-top: 5px solid $tr-brand-color !important;
+
     .icon {
         color: white;
         background-color: $tr-brand-salmon;
@@ -35,6 +39,11 @@ export default {
     a {
         color: inherit;
         text-decoration: none;
+        text-align: center;
+
+        font-weight: 700;
+        font-size: 1.25rem;
+
     }
 
 }
