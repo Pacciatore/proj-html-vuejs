94a74b4957900ea786378a9469021bb43a0a52de
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-10 00:05:51 +0200

    creazione component per le card di ForumSections

---
+++ src/components/contentUtils/ForumCardComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="card">
+
+        <a href="#">News & Community</a>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ForumCardComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+.card {
+
+    a {
+        color: inherit;
+        text-decoration: none;
+    }
+
+}
+</style>
