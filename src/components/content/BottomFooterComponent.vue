aac80cfeae63a33e461f629fc8a3a229e6b00b0b
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-10 15:35:22 +0200

    impostazione bottomFooter

--- src/components/content/BottomFooterComponent.vue
+++ src/components/content/BottomFooterComponent.vue
@@ -1,6 +1,28 @@
 <template>
-    <div>
+    <div class="py-5">
+        <div class="container d-flex justify-content-between">
 
+            <!-- Colonna delle note legali -->
+            <div class="legal-notes col-6 d-flex">
+
+                <div class="col">Copyright</div>
+                <div class="col">Avada</div>
+                <div class="col">All Rights</div>
+                <div class="col">Powered by</div>
+
+            </div>
+
+            <!-- Colonna per i link social -->
+            <div class="social-links col-3 d-flex justify-content-end">
+
+                <font-awesome-icon icon="fa-brands fa-facebook-f" />
+                <font-awesome-icon icon="fa-brands fa-twitter" />
+                <font-awesome-icon icon="fa-brands fa-instagram" />
+                <font-awesome-icon icon="fa-brands fa-youtube" />
+
+            </div>
+
+        </div>
     </div>
 </template>
 
@@ -11,5 +33,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/assets/style/variables.scss';
 
+div.py-5 {
+    border-top: 1px solid $tr-custom-grey;
+}
 </style>
