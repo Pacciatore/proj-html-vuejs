414d82b16cf4904e86b84c75b0eadc86bc10739c
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-10 12:59:08 +0200

    uso di joinUsComponent

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -7,7 +7,7 @@
     <BlackBannerComponent :text="'Read our blog'" />
     <ForumSectionsComponent />
     <BlackBannerComponent :text="'Go to our forum'" />
-
+    <JoinUsComponent />
 
   </main>
 </template>
@@ -18,9 +18,10 @@ import AvadaMessageComponent from './content/AvadaMessageComponent.vue';
 import LatestNewsComponent from './content/LatestNewsComponent.vue';
 import BlackBannerComponent from './contentUtils/BlackBannerComponent.vue';
 import ForumSectionsComponent from './content/ForumSectionsComponent.vue';
+import JoinUsComponent from './content/JoinUsComponent.vue';
 export default {
   name: "MainComponent",
-  components: { JumboComponent, AvadaMessageComponent, LatestNewsComponent, BlackBannerComponent, ForumSectionsComponent }
+  components: { JumboComponent, AvadaMessageComponent, LatestNewsComponent, BlackBannerComponent, ForumSectionsComponent, JoinUsComponent }
 }
 </script>
 
