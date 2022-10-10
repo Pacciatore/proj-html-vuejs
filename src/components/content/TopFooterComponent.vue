df746baa6fd60363bf6a70d523f99d82cdee6672
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-10-10 13:41:36 +0200

    uso di props

--- src/components/content/TopFooterComponent.vue
+++ src/components/content/TopFooterComponent.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="top-footer">
 
-        <TopFooterElementComponent />
+        <div class="container d-flex">
 
+            <div v-for="element in footerElements" :key="element.title">
+                <TopFooterElementComponent :element="element" />
+            </div>
+
+        </div>
     </div>
 </template>
 
@@ -10,6 +15,28 @@
 import TopFooterElementComponent from '../contentUtils/TopFooterElementComponent.vue';
 export default {
     name: "TopFooterComponent",
+    data() {
+        return {
+            footerElements: [
+                {
+                    title: 'Avada Tech Forum',
+                    questions: ['Sed semper, nisl id feugiat eleifend, erat est digssim felis, quis auctor elit enim nec ante. Cras et nibh risus. Pellentesque non mi sit amet mi intermentum.']
+                },
+                {
+                    title: 'Popular Topics',
+                    questions: ['Sed semper, nisl id feugiat eleifend, erat est digssim felis, quis auctor elit enim nec ante. Cras et nibh risus. Pellentesque non mi sit amet mi intermentum.']
+                },
+                {
+                    title: 'Recent Topics',
+                    questions: ['Sed semper, nisl id feugiat eleifend, erat est digssim felis, quis auctor elit enim nec ante. Cras et nibh risus. Pellentesque non mi sit amet mi intermentum.']
+                },
+                {
+                    title: 'Latest Replies',
+                    questions: ['Sed semper, nisl id feugiat eleifend, erat est digssim felis, quis auctor elit enim nec ante. Cras et nibh risus. Pellentesque non mi sit amet mi intermentum.']
+                }
+            ]
+        }
+    },
     components: { TopFooterElementComponent }
 }
 </script>
--- src/components/contentUtils/TopFooterElementComponent.vue
+++ src/components/contentUtils/TopFooterElementComponent.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
 
+        <h6>{{ element.title }}</h6>
+
+        <!-- v-if -->
+        <p>Sedembfisdbvhjdskb</p>
+
     </div>
 </template>
 
 <script>
 export default {
-    name: 'TopFooterElementComponent'
+    name: 'TopFooterElementComponent',
+    props: {
+        element: Object
+    }
 }
 </script>
 
